ed1fa0f05371fba80030c3fea8ccfc409818f7a5
NolanZhao <z_nolan@126.com>
2017-07-19 11:26:15 +0800

    init

---
+++ LeetcodePython/110.py
@@ -0,0 +1,51 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def maxDepth(self, root):
+        if root is None:
+            return 0
+        else:
+            return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1                          
+        
+    
+    def isBalanced(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        if root == None:
+            return True
+        else:
+            if abs(self.maxDepth(root.left) - self.maxDepth(root.right)) > 1:
+                return False
+            else:
+                return self.isBalanced(root.left) and self.isBalanced(root.right)
+
+
+# 2
+class Solution2(object):
+    def isBalanced(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        balanced, _ = self.validate(root)
+        return balanced
+        
+    def validate(self, root):
+        if root is None:
+            return True, 0
+            
+        balanced, leftHeight = self.validate(root.left)
+        if not balanced:
+            return False, 0
+        balanced, rightHeight = self.validate(root.right)
+        if not balanced:
+            return False, 0
+            
+        return abs(leftHeight - rightHeight) <= 1, max(leftHeight, rightHeight) + 1
